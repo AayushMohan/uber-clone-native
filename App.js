2ca182a008a5413c07d92c9ec0f0e8f907abc6d3
AayushMohan <aayushmohan1702@gmail.com>
2022-07-20 16:39:54 +0530

    Added HomeScreen.js and tailwind css

--- App.js
+++ App.js
@@ -2,13 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import HomeScreen from "./screens/homeScreen";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Let's build UBER!</Text>
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
